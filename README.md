65285d57d3e9bab41db50464770b7fcc0abe977a
Casian Lacatusu <casian.lacatusu@gmail.com>
2019-04-22 23:40:42 +0300

    update

---
+++ README.md
@@ -0,0 +1,3 @@
+# gogo - your Golang CLI quick starter!
+
+We're all lazy ... why bother setting up the boilerplate every single time?
--- main.go
+++ main.go
@@ -7,7 +7,13 @@ import (
 	"github.com/kwix/gogo/commands"
 )
 
+var (
+	buildVersion string
+)
+
 func main() {
+	commands.GogoCmd.Version = buildVersion
+
 	if err := commands.GogoCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
